d71d7234695ee49bf14b3fc671b98425cd72f858
Le Quoc Tri <quoctri2106@gmail.com>
2025-02-13 12:59:05 +0700

    add ui exit

---
+++ Assets/_Data/UI/Button/BaseButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class BaseButton : TrisMonoBehaviour
+{
+    [SerializeField] protected Button button;
+    protected override void Start()
+    {
+        base.Start();
+        this.AddOnClickEvent();
+    }
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadButton();
+    }
+    protected virtual void LoadButton()
+    {
+        if (button != null) return;
+        this.button = GetComponent<Button>();
+        Debug.Log(transform.name + " :LoadButton", gameObject);
+    }
+    protected virtual void AddOnClickEvent()
+    {
+        this.button.onClick.AddListener(this.OnClick);
+    }
+    protected abstract void OnClick();
+    
+}
